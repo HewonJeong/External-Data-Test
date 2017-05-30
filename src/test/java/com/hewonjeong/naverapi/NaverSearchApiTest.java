94f20baa9485f93f0fe73e0ec54d39aae3e38f6c
hewongjeong <hewon.jng@gmail.com>
2017-05-30 16:49:05 +0900

    Change stop condition to prevent useless request

--- src/test/java/com/hewonjeong/naverapi/NaverSearchApiTest.java
+++ src/test/java/com/hewonjeong/naverapi/NaverSearchApiTest.java
@@ -7,26 +7,65 @@ import org.json.JSONObject;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class NaverSearchApiTest {
     final int EXPECTED_ITEMS_LENGTH = 1000;
-    final String[] KIN_OBJECT_KEYS = {"link", "description", "title"};
+    final String[] OBJECT_KEYS = {"link", "description", "title"};
+    final String[] TARGET_DOMAINS = {"blog", "local", "kin", "news"};
 
     @Test
     public void testRequest_VaildParms_ShouldPass() {
-        JSONObject res = NaverSearchApi.request("kin", "춘천 여행", 100, 1, "sim");
-        JSONArray items = (JSONArray)res.get("items");
-        assertTrue(items.length() == EXPECTED_ITEMS_LENGTH);
-        for(int i = 0; i < items.length(); i++) {
-            JSONObject obj = items.getJSONObject(i);
-            assertTrue(hasKeys(obj, KIN_OBJECT_KEYS));
+        for (String s : TARGET_DOMAINS) {
+            JSONObject res = NaverSearchApi.request(s, "춘천 여행", 100, 1, "sim");
+            JSONArray items = (JSONArray) res.get("items");
+            assertTrue(items.length() == EXPECTED_ITEMS_LENGTH);
+            for (int i = 0; i < items.length(); i++) {
+                JSONObject obj = items.getJSONObject(i);
+                assertTrue(hasKeys(obj, OBJECT_KEYS));
+            }
         }
     }
+
     @Test
-    public void testRequest_invaildParm_ShouldPass() {
+    public void testRequest_InvaildDomainValue_ShouldPass() {
         JSONObject res = NaverSearchApi.request("WRONG_PARM", "춘천 여행", 100, 1, "sim");
+        assertEquals("Error", res.getString("result"));
         assertEquals(ApiErrorCode.NAVER_API_ERROR, res.getString("errorCode"));
+    }
+
+    @Test
+    public void testRequest_NoResultQuery_ShouldPass() {
+        for (String s : TARGET_DOMAINS) {
+            final String NO_RESULT_QUERY = "Ffi22jp33#$#@d";
+            JSONObject res = NaverSearchApi.request(s, NO_RESULT_QUERY, 100, 1, "sim");
+            System.out.println(res);
+            assertFalse(res.has("errorCode"));
+            assertEquals("OK", res.getString("result"));
+            JSONArray items = (JSONArray) res.get("items");
+            assertTrue(items.length() == 0);
+        }
+    }
+
+    @Test
+    public void testRequest_InvalidDisplayValue_ShouldPass() {
+        final int[] wrongDisplayVals = {-1, 0, 101};
+        for (int val : wrongDisplayVals) {
+            JSONObject res = NaverSearchApi.request("blog", "전주 여행", val, 1, "sim");
+            assertEquals("Error", res.getString("result"));
+            assertEquals(ApiErrorCode.NAVER_API_ERROR, res.getString("errorCode"));
+        }
+    }
+
+    @Test
+    public void testRequest_InvalidStartValue_ShouldPass() {
+        final int[] wrongStartVals = {-1, 0, 1001};
+        for (int val : wrongStartVals) {
+            JSONObject res = NaverSearchApi.request("blog", "전주 여행", 100, 1001, "sim");
+            System.out.println(res.toString());
+            assertEquals(ApiErrorCode.NAVER_API_ERROR, res.getString("errorCode"));
+        }
     }
 
     private boolean hasKeys(JSONObject obj, String[] keys) {
